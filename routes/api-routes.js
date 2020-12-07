cb64bafa1577852e4de7063b0110b28c916c048f
EWager <ethanbwager@gmail.com>
2020-12-07 12:28:53 -0500

    test

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,7 @@
 const path = require("path");
-const savedNotes = require('../db/db.json')
+const savedNotes = require("../db/db.json");
+const fs = require("fs");
+const { dirname } = require("path");
 
 //sets up file paths for /notes and /everythingElse to send to pages.
 module.exports = function (app) {
@@ -8,9 +10,18 @@ module.exports = function (app) {
     res.json(savedNotes);
   });
 
-  app.post('/api/notes', function (req, res) {
-    savedNotes.push(req.body)
-    res.json({savedNotes });
-  });
+  app.post("/api/notes", function (req, res) {
+    savedNotes.push(req.body); //this pushes the new data to api/notes
+    res.json({ savedNotes });
 
+    fs.writeFile(
+      __dirname + "../db/db.json",
+      JSON.stringify(savedNotes),
+      function writeJSON(err) {
+        if (err) return console.log(err);
+        console.log(JSON.stringify(file));
+        console.log("writing to " + fileName);
+      }
+    );
+  });
 };
